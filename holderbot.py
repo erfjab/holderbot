affcb0786dfb5ae4d41414e2edde5e7dd8d03c33
Erfan <143827987+erfjab@users.noreply.github.com>
2024-01-15 03:43:26 +0330

    fix proxies

--- holderbot.py
+++ holderbot.py
@@ -2,9 +2,9 @@ from pyrogram import *
 from pyrogram.types import *
 from pyrogram.errors.exceptions import *
 from io import *
-import requests , json , time , uuid , qrcode , html , re
+import requests , json , time , qrcode , html , re
 #---------------------------------input data---------------------------------
-with open('config.json', 'r') as file:
+with open('0-files/holderbot/config.json', 'r') as file:
     config = json.load(file)
 admin_telegram_bot = config['admin_telegram_bot']
 telegram_bot_token = config['telegram_bot_token']
@@ -23,14 +23,9 @@ try:
             'accept': 'application/json',
             'Content-Type': 'application/json',
             'Authorization': f"Bearer {access_token}"}
-        print("\n\n\n\n\n\n\n\n\n\nBOT IS STARTED...\n\n\n\n\n")
     else :
         print(panel_token.text)
-        print("\n\n\n\n\n\n\n\n\n\nError...\n\n\n\n\n")
-        print("i can't create acces token , please try again and enter true info or contact with spport @erfjab")
         time.sleep(1)
-        print("\n\n\n\n\n\n\n\n\n\nPress Ctrl + C and go to github\n\n\n\n\n")
-        time.sleep(10)
 except Exception as e:
     print(f"i have a error : {str(e)}")
     time.sleep(10)
@@ -96,68 +91,83 @@ async def create(client: Client, message: Message) :
 #---------------------------------telegram fallback & create user---------------------------------
 @app.on_callback_query()
 async def handle_callback_query(client: Client, query: CallbackQuery):
-    data = query.data
-    if data == "yes":
-        await query.edit_message_text(text="<b>OK, please wait...</b>" , parse_mode=enums.ParseMode.HTML)
-        global user_name , user_data , user_date 
-        if user_data.lower() == "unlimited" :
-            bytedata = 0
-        else :
-            bytedata = int(user_data) * (1024**3)
-        daysdata = user_date * (24*60*60)
-        url = f"https://{marzban_panel_domain}/api/user"
-        newuuid = uuid.uuid4()
-        data={
-        "username": user_name,
-        "proxies": {},
-        "inbounds": {},
-        "expire" : 0,
-        "data_limit": bytedata,
-        "data_limit_reset_strategy": "no_reset",
-        "status": "on_hold",
-        "note": "",
-        "on_hold_timeout": "2024-11-03T20:30:00",
-        "on_hold_expire_duration": daysdata
-        }
-        data = json.dumps(data)
-        responce = requests.post(url=url , headers=panel_headers , data=data)
-        await query.message.delete()
-        if responce.status_code == 200 :
-            url = f"https://{marzban_panel_domain}/api/user/{user_name}"
-            responce = requests.get(url=url , headers=panel_headers)
-            if responce.status_code == 200 :
-                datauser = json.loads(responce.text)
-                subscription_url = datauser.get('subscription_url')
-                qr = qrcode.QRCode(
-                    version=1,
-                    error_correction=qrcode.constants.ERROR_CORRECT_L,
-                    box_size=10,
-                    border=4,
-                )
-                qr.add_data(subscription_url)
-                qr.make(fit=True)
-                qr_img = qr.make_image(fill_color="black", back_color="white")
-                img_bytes_io = BytesIO()
-                qr_img.save(img_bytes_io, 'PNG')
-                img_bytes_io.seek(0)
-                await app.send_photo(chat_id=admin_telegram_bot , photo=img_bytes_io,caption=f"<code>{html.escape(subscription_url)}</code>")
-                await client.send_message(chat_id=admin_telegram_bot , text=f"<b>✅ {user_name} | {user_data} GB | {user_date} Days</b>" , parse_mode=enums.ParseMode.HTML) 
-                global sendgithub
-                if sendgithub == 0 :
-                    text = '<b>❤️ If you like it, then give us a <a href="https://github.com/erfjab/holderbot">star on GitHub</a></b>\n(You will not see this message again)'
-                    await client.send_message(chat_id=admin_telegram_bot , text=text , parse_mode=enums.ParseMode.HTML) 
-                    sendgithub = 1
+    global user_name , user_data , user_date 
+    try :
+        data = query.data
+        if data == "yes":
+            await query.edit_message_text(text="<b>OK, please wait...</b>" , parse_mode=enums.ParseMode.HTML)
+            #---------------check inound---------------
+            inbounschecker = requests.get(url=f"https://{marzban_panel_domain}/api/inbounds" , headers=panel_headers)
+            if inbounschecker.status_code == 200 :
+                proxylist = {}
+                inbounscheckertext = inbounschecker.text.lower()
+                protocols = ["trojan" , "vmess" , "vless" , "shadowsocks"]
+                for i in protocols :
+                    if i in inbounscheckertext :
+                        proxylist[i] = {}
+            else : 
+                await query.message.delete()
+                await client.send_message(chat_id=admin_telegram_bot , text="<b>❌ i can't check inbounds !</b>\n<code>{str(responce.text)}</code>" , parse_mode=enums.ParseMode.HTML) 
+                return
+            if user_data.lower() == "unlimited" :
+                bytedata = 0
             else :
-                text = f"<b>❌ i cerated user but I can't send it to you!</b>"
-                await client.send_message(chat_id=admin_telegram_bot , text=text , parse_mode=enums.ParseMode.HTML)   
+                bytedata = int(user_data) * (1024**3)
+            daysdata = user_date * (24*60*60)
+            url = f"https://{marzban_panel_domain}/api/user"
+            data={
+            "username": user_name,
+            "proxies" : proxylist,
+            "inbounds": {},
+            "expire" : 0,
+            "data_limit": bytedata,
+            "data_limit_reset_strategy": "no_reset",
+            "status": "on_hold",
+            "note": "",
+            "on_hold_timeout": "2024-11-03T20:30:00",
+            "on_hold_expire_duration": daysdata
+            }
+            data = json.dumps(data)
+            responce = requests.post(url=url , headers=panel_headers , data=data)
+            await query.message.delete()
+            if responce.status_code == 200 :
+                url = f"https://{marzban_panel_domain}/api/user/{user_name}"
+                responce = requests.get(url=url , headers=panel_headers)
+                if responce.status_code == 200 :
+                    datauser = json.loads(responce.text)
+                    subscription_url = datauser.get('subscription_url')
+                    qr = qrcode.QRCode(
+                        version=1,
+                        error_correction=qrcode.constants.ERROR_CORRECT_L,
+                        box_size=10,
+                        border=4,
+                    )
+                    qr.add_data(subscription_url)
+                    qr.make(fit=True)
+                    qr_img = qr.make_image(fill_color="black", back_color="white")
+                    img_bytes_io = BytesIO()
+                    qr_img.save(img_bytes_io, 'PNG')
+                    img_bytes_io.seek(0)
+                    await app.send_photo(chat_id=admin_telegram_bot , photo=img_bytes_io,caption=f"<code>{html.escape(subscription_url)}</code>")
+                    await client.send_message(chat_id=admin_telegram_bot , text=f"<b>✅ {user_name} | {user_data} GB | {user_date} Days</b>" , parse_mode=enums.ParseMode.HTML) 
+                    global sendgithub
+                    if sendgithub == 0 :
+                        text = '<b>❤️ If you like it, then give us a <a href="https://github.com/erfjab/holderbot">star on GitHub</a></b>\n(You will not see this message again)'
+                        await client.send_message(chat_id=admin_telegram_bot , text=text , parse_mode=enums.ParseMode.HTML) 
+                        sendgithub = 1
+                else :
+                    text = f"<b>❌ i cerated user but I can't send it to you!</b>"
+                    await client.send_message(chat_id=admin_telegram_bot , text=text , parse_mode=enums.ParseMode.HTML)   
+            else :
+                text = f"<b>❌ i can't create user !</b>\n<code>{str(responce.text)}</code>"
+                await client.send_message(chat_id=admin_telegram_bot , text=text , parse_mode=enums.ParseMode.HTML) 
         else :
-            text = f"<b>❌ i can't create user !</b>\n<code>{str(responce.text)}</code>"
-            await client.send_message(chat_id=admin_telegram_bot , text=text , parse_mode=enums.ParseMode.HTML) 
-    else :
-        message_id = int(data.split()[1])
-        await query.edit_message_text(text="<b>OK, I forgot...</b>" , parse_mode=enums.ParseMode.HTML)
-        time.sleep(2)
-        await query.message.delete()
-        await client.delete_messages(chat_id=admin_telegram_bot, message_ids=message_id)
-
+            message_id = int(data.split()[1])
+            await query.edit_message_text(text="<b>OK, I forgot...</b>" , parse_mode=enums.ParseMode.HTML)
+            time.sleep(2)
+            await query.message.delete()
+            await client.delete_messages(chat_id=admin_telegram_bot, message_ids=message_id)
+    except Exception as e :
+        error_message = f"<b>❌ Error :</b>\n<code>{str(e)}</code>"
+        await client.send_message(chat_id=admin_telegram_bot, text=error_message, parse_mode=enums.ParseMode.HTML) 
 app.run()
