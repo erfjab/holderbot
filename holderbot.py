75546029f0c5fb9730e85a0afd96da1496824f0e
Erfan <143827987+erfjab@users.noreply.github.com>
2024-01-15 03:50:59 +0330

    Update holderbot.py

--- holderbot.py
+++ holderbot.py
@@ -4,7 +4,7 @@ from pyrogram.errors.exceptions import *
 from io import *
 import requests , json , time , qrcode , html , re
 #---------------------------------input data---------------------------------
-with open('0-files/holderbot/config.json', 'r') as file:
+with open('config.json', 'r') as file:
     config = json.load(file)
 admin_telegram_bot = config['admin_telegram_bot']
 telegram_bot_token = config['telegram_bot_token']
@@ -51,17 +51,12 @@ async def start(client: Client, message: Message) :
     if chatid != admin_telegram_bot :
         await client.send_message(chat_id=chatid , text=f"<b>this bot is private...</b>" , parse_mode=enums.ParseMode.HTML)
     if chatid == admin_telegram_bot :
-        text = f"<b>hello Dear {firstname}\nTo create a user, please use this template 👇🏻 :</b>\n\n<b>Username, data(GB), date(days)</b>\n\n<code>example: alex 20 30\nexample: maria unlimited 120\nexample: kevin unlimited 80</code> \n\n<b>💬 if you need help : @erfjab\n⭐ What is premium holderbot? click /pro</b>"
+        text = f"<b>hello Dear {firstname}(Version : 1.3.2)\nTo create a user, please use this template 👇🏻 :</b>\n\n<b>Username, data(GB), date(days)</b>\n\n<code>example: alex 20 30\nexample: maria unlimited 120\nexample: kevin unlimited 80</code> \n\n<b>💬 if you need help ? please issue in Github</b>"
         await client.send_message(chat_id=admin_telegram_bot , text=text , parse_mode=enums.ParseMode.HTML)
 #---------------------------------telegram pattern command---------------------------------
 @app.on_message(filters.private & filters.command("pattern") & filters.user(admin_telegram_bot) )
 async def pattern(client: Client, message: Message) :
-    text = "<b>USERNAME VOLUME TIME\n\n◽ username : A-Z 0-9 3-32 character\n◽ volume : unlimited or Number(int)\n◽ time : Number(int)</b>\n\n<code>example: anna 50 30 \nexample: jordi unlimited 120\nexample: erfjab unlimited 30</code> \n\n<b>💬 if you need help : @erfjab</b>"
-    await client.send_message(chat_id=admin_telegram_bot , text=text , parse_mode=enums.ParseMode.HTML)
-#---------------------------------telegram pro command---------------------------------
-@app.on_message(filters.private & filters.command("pro") & filters.user(admin_telegram_bot))
-async def pro(client: Client, message: Message) :
-    text = "<b>😍 Premium features :\n\n🔹 Get the pro statistics bot with customize\n🔹 Create admin with volume and time limits\n🔹 Upload the link to the channel/group/PV\n🔹 Create a group of users\n🔹 Desired change of inbound configs\n🔹 Restart user usage\n🔹 Change user volume and time\n🔹 Can reconnect nodes\n🔹 And whatever you want... \n\n💬 if you want premium holderbot or need help , contact me : @erfjab</b>"
+    text = "<b>USERNAME VOLUME TIME\n\n◽ username : A-Z 0-9 3-32 character\n◽ volume : unlimited or Number(int)\n◽ time : Number(int)</b>\n\n<code>example: anna 50 30 \nexample: jordi unlimited 120\nexample: erfjab unlimited 30</code> \n\n<b>💬 if you need help ? please issue in Github</b>"
     await client.send_message(chat_id=admin_telegram_bot , text=text , parse_mode=enums.ParseMode.HTML)
 #---------------------------------telegram create command---------------------------------
 @app.on_message(filters.private & filters.user(admin_telegram_bot))
