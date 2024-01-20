dd316846a484571568e21aafa1e870be7cad2f63
Erfan <143827987+erfjab@users.noreply.github.com>
2024-01-21 01:17:46 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
+# ورود به روت 
+cd && cd ..
+
 # چک کردن نصب بودن Python، Git و pip3
 if ! command -v python3 &> /dev/null; then
     echo "Python not found. Installing..."
-    sudo apt-get update
     sudo apt-get install -y python3
 fi
 
 if ! command -v git &> /dev/null; then
     echo "Git not found. Installing..."
-    sudo apt-get update
     sudo apt-get install -y git
 fi
 
 if ! command -v pip3 &> /dev/null; then
     echo "pip3 not found. Installing..."
-    sudo apt-get update
     sudo apt-get install -y python3-pip
 fi
 
@@ -25,21 +25,13 @@ if ps aux | grep -v grep | grep "python3 holderbot.py" &> /dev/null; then
     pkill -f "python3 holderbot.py"
 fi
 
-# چک کردن وجود فایل install.sh و حذف آن
-if [ -e "install.sh" ]; then
-    echo "install.sh file already exists. Deleting..."
-    rm install.sh
-fi
-
 # چک کردن وجود پوشه holder و حذف آن
 if [ -d "holder" ]; then
     echo "Directory holder already exists. Deleting..."
     rm -rf holder
 fi
 
-
 pkill -x "python3 holderbot.py"
-
 
 mkdir holder
 cd holder
@@ -57,7 +49,6 @@ python3 -m venv venv
 source venv/bin/activate
 
 # نصب پیش‌نیازها
-pip install -r requirements.txt
 pip install -U pyrogram tgcrypto
 
 # اطلاعات را از کاربر بخوان
@@ -69,7 +60,7 @@ read -p "Please enter your marzban panel sub domain name (www.example.com): " ma
 
 # اطلاعات را در یک فایل JSON بنویس
 echo "{
-    \"admin_telegram_bot\": $admin_telegram_bot,
+    \"admin_telegram_bot\": \"$admin_telegram_bot\",
     \"telegram_bot_token\": \"$telegram_bot_token\",
     \"marzban_panel_username\": \"$marzban_panel_username\",
     \"marzban_panel_password\": \"$marzban_panel_password\",
@@ -77,4 +68,4 @@ echo "{
 }" > config.json
 
 chmod +x holderbot.py
-nohup python3 holderbot.py
+nohup python3 holderbot.py &
