9665de1cf6150d5632ae8f26d83c6d5b3e4dbf7d
Erfan <143827987+erfjab@users.noreply.github.com>
2024-02-02 05:24:35 +0330

    Update start_message.py

--- plugins/start_message.py
+++ plugins/start_message.py
@@ -5,17 +5,24 @@ from datetime import datetime, timezone
 from persiantools.jdatetime import JalaliDateTime
 from io import *
 from dateutil import tz
-import requests , json , time , qrcode , html , re , pytz
+import requests , json , time , qrcode , html , re , pytz , sqlite3
 
 #------------------------------------- JSON INFO -------------------------------------#
 
-with open('config.json', 'r', encoding='utf-8') as file:
-    CONFIG = json.load(file)
-
-ADMIN_TGBOT = int(CONFIG['admin_telegram_bot'])
-PANEL_USER = CONFIG['marzban_panel_username']
-PANEL_PASS = CONFIG['marzban_panel_password']
-PANEL_DOMAIN = CONFIG['marzban_panel_domain']
+conn = sqlite3.connect('holder.db')
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM config ORDER BY id DESC LIMIT 1")
+row = cursor.fetchone()
+if row:
+    ADMIN_TGBOT = int(row[1])
+    PANEL_USER = row[2]
+    PANEL_PASS = row[3]
+    PANEL_DOMAIN = row[4]
+    TOKEN_TGBOT = row[5]
+else:
+    print("No configuration found in the database. Exiting.")
+    exit()
+conn.close()
 
 #------------------------------------- ON MESSAGE & ONE LEN MESSAGE -------------------------------------#
 
