3cedcc9d310ba6a329bf2caba36556c0be48d03d
Erfan <143827987+erfjab@users.noreply.github.com>
2024-02-02 05:33:20 +0330

    Update holder.py

--- holder.py
+++ holder.py
@@ -5,9 +5,20 @@ from io import *
 import json
 
 #------------------------- IMPORT INFO -------------------------#
-with open('config.json', 'r') as file:
-    config = json.load(file)
-telegram_bot_token = config['telegram_bot_token']
+conn = sqlite3.connect('holder.db')
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM config ORDER BY id DESC LIMIT 1")
+row = cursor.fetchone()
+if row:
+    ADMIN_TGBOT = int(row[1])
+    PANEL_USER = row[2]
+    PANEL_PASS = row[3]
+    PANEL_DOMAIN = row[4]
+    TOKEN_TGBOT = row[5]
+else:
+    print("No configuration found in the database. Exiting.")
+    exit()
+conn.close()
 
 #------------------------- SET BOT  -------------------------#
 plugins = dict(root="plugins")
