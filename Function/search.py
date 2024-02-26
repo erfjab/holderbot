210dc3c8b1324bbf054c41c1f64f520a63549762
Erfan <143827987+erfjab@users.noreply.github.com>
2024-02-27 01:16:08 +0330

    Update search.py

--- Function/search.py
+++ Function/search.py
@@ -10,13 +10,13 @@ def DEF_SEARCH_USERS (CHATID , MESSAGE_TEXT):
     RESPONCE = requests.get(url=URL , headers=PANEL_TOKEN)
     if RESPONCE.status_code == 200 :
         RESPONCE_DATA = RESPONCE.json()
-        USERS = [user.get('username').lower() for user in RESPONCE_DATA.get('users', [])]
-        similarity_scores = [(user, SequenceMatcher(None, MESSAGE_TEXT.lower(), user).ratio()) for user in USERS]
+        USERS = [user.get('username') for user in RESPONCE_DATA.get('users', [])]
+        similarity_scores = [(user, SequenceMatcher(None, MESSAGE_TEXT, user).ratio()) for user in USERS]
         similar_users = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
         similar_users = [user for user, score in similar_users if score > 0.55]
         if similar_users:
             similar_users_text = "<b>,</b> ".join([f"<code>{user}</code>" for user in similar_users])
-            TEXT = f"<b>Did you mean?</b> {similar_users_text}"
+            TEXT = f"<b>Did you mean?</b> {similar_users_text}\n\n<b>❕Return to the home page to receive user statistics.</b>"
         else :
             TEXT = f"<b>❌ I can't find any user.</b>"
     else :
