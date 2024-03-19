21e9204b531227dd6434ce2581e6642ccce6db33
Erfan <143827987+erfjab@users.noreply.github.com>
2024-03-19 20:30:21 +0330

    Update keyboards.py

--- Function/keyboards.py
+++ Function/keyboards.py
@@ -15,7 +15,7 @@ KEYBOARD_CANCEL = ReplyKeyboardMarkup([[KeyboardButton("🔙 cancel")]] , resize
 def KEYBOARD_ADMINS_LIST(CHATID) :
     PANEL_USER, PANEL_PASS, PANEL_DOMAIN = DEF_IMPORT_DATA (CHATID)
     PANEL_TOKEN = DEF_PANEL_ACCESS(PANEL_USER, PANEL_PASS, PANEL_DOMAIN)
-    URL = f"https://{PANEL_DOMAIN}/api/admins"
+    URL = f"{PANEL_DOMAIN}/api/admins"
     RESPONCE = requests.get(url=URL, headers=PANEL_TOKEN , verify=False)
     if RESPONCE.status_code == 200:
         RESPONCE_DATA = RESPONCE.json()
@@ -51,7 +51,7 @@ KEYBOARD_LIST_TIMES = ReplyKeyboardMarkup([[KeyboardButton("1 min"), KeyboardBut
 def DEF_NODES_LIST(CHATID) :
     PANEL_USER, PANEL_PASS, PANEL_DOMAIN = DEF_IMPORT_DATA (CHATID)
     PANEL_TOKEN = DEF_PANEL_ACCESS(PANEL_USER, PANEL_PASS, PANEL_DOMAIN)
-    URL = f"https://{PANEL_DOMAIN}/api/nodes"
+    URL = f"{PANEL_DOMAIN}/api/nodes"
     RESPONCE = requests.get(url=URL, headers=PANEL_TOKEN , verify=False)
     if RESPONCE.status_code == 200:
         RESPONCE_DATA = RESPONCE.json()
