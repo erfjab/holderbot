5b4e304ce8b6cf0bc4146d049d6f302cd48f1ad2
erfjab <erfjab@gmail.com>
2024-12-08 18:22:09 +0330

    feat(settings/log): add logger manager

--- settings/__init__.py
+++ settings/__init__.py
@@ -1,9 +1,11 @@
 from .env.config import EnvSetup
 from .lang.keyboard import KeyboardTextSetup
 from .lang.message import MessageTextSetup
+from .log.log import LoggerSetup
 
 EnvFile = EnvSetup()
 MessageText = MessageTextSetup()
 KeyboardText = KeyboardTextSetup()
+logger = LoggerSetup("HolderBot").get_logger()
 
-__all__ = ["EnvFile", "MessageText", "KeyboardText"]
+__all__ = ["EnvFile", "MessageText", "KeyboardText", "logger"]
---
+++ settings/log/log.py
@@ -0,0 +1,20 @@
+import logging
+
+class LoggerSetup:
+    """setup projcet logger"""
+    def __init__(self, name: str, level: int = logging.INFO):
+        self.bot_logger = logging.getLogger(name)
+        self.bot_logger.setLevel(level)
+
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.DEBUG)
+
+        formatter = logging.Formatter(
+            "%(asctime)-25s | %(levelname)-8s | %(message)s", "%Y-%m-%d %H:%M:%S"
+        )
+        console_handler.setFormatter(formatter)
+
+        self.bot_logger.addHandler(console_handler)
+
+    def get_logger(self):
+        return self.bot_logger
