c3e4bdb64ede657458a52c68461a78a5b90080bf
erfjab <erfjab@gmail.com>
2025-01-06 15:16:24 +0330

    improve: speedup create qr

--- app/settings/utils/qrcode.py
+++ app/settings/utils/qrcode.py
@@ -1,50 +1,57 @@
 import qrcode
+from functools import lru_cache
 from io import BytesIO
 from PIL import Image
 from ..config import env
+
+
+@lru_cache(maxsize=1)
+def get_processed_background(background_path):
+    """Cache the background image with minimal processing"""
+    if not background_path:
+        return None
+
+    with Image.open(background_path) as img:
+        return img.convert("RGB")
 
 
 async def create_qr(text: str) -> bytes:
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=3,
+        box_size=6,
+        border=1,
     )
     qr.add_data(text)
     qr.make(fit=True)
-
-    qr_img = qr.make_image(fill_color="black", back_color="white").convert("RGBA")
+    qr_img = qr.make_image(fill_color="black", back_color="white").convert("L")
 
     backphoto = env.QR_BACKGROUND
     if backphoto:
-        background = Image.open(backphoto).convert("RGBA")
+        background = get_processed_background(backphoto)
+        if background is None:
+            img_bytes_io = BytesIO()
+            qr_img.save(img_bytes_io, "JPEG", quality=50)
+            return img_bytes_io.getvalue()
 
-        max_size = (800, 800)
-        if background.size[0] > max_size[0] or background.size[1] > max_size[1]:
-            background.thumbnail(max_size, Image.LANCZOS)
+        new_size = (background.size[0] // 2, background.size[1] // 2)
+        qr_img_resized = qr_img.resize(new_size, Image.NEAREST)
 
-        new_size = tuple(int(0.65 * min(background.size)) for _ in range(2))
-        qr_img_resized = qr_img.resize(new_size, Image.LANCZOS)
+        position = (
+            (background.size[0] - new_size[0]) >> 1,
+            (background.size[1] - new_size[1]) >> 1,
+        )
 
-        position = tuple((background.size[i] - new_size[i]) // 2 for i in range(2))
+        final_img = background.copy()
 
-        white_background = Image.new("RGBA", qr_img_resized.size, (255, 255, 255, 255))
+        white_bg = Image.new("L", new_size, 255)
+        final_img.paste(white_bg, position)
+        final_img.paste(qr_img_resized, position)
 
-        final_img = Image.new("RGBA", background.size, (0, 0, 0, 0))
-        final_img.paste(background, (0, 0))
-
-        final_img.paste(white_background, position, white_background)
-        final_img.paste(qr_img_resized, position, qr_img_resized)
     else:
         final_img = qr_img
 
     img_bytes_io = BytesIO()
+    final_img.save(img_bytes_io, "JPEG", quality=50, optimize=False, progressive=False)
 
-    final_img.save(
-        img_bytes_io,
-        "PNG",
-        optimize=True,
-        quality=60,
-    )
     return img_bytes_io.getvalue()
