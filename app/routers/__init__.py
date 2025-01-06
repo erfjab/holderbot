12099994efd4ecb4860131d4020b7bc3f73a1a01
erfjab <erfjab@gmail.com>
2025-01-07 02:39:09 +0330

    feat: add inline router

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,6 +1,6 @@
 from aiogram import Router
 
-from . import base
+from . import base, inline
 from .servers import setup_server_routers
 from .users import setup_user_routers
 from .actions import setup_action_routers
@@ -10,12 +10,14 @@ __all__ = [
     "setup_server_routers",
     "setup_action_routers",
     "base",
+    "inline",
 ]
 
 
 def setup_routers() -> Router:
     router = Router()
     router.include_router(base.router)
+    router.include_router(inline.router)
     router.include_router(setup_server_routers())
     router.include_router(setup_user_routers())
     router.include_router(setup_action_routers())
