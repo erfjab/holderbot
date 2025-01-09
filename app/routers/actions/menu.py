8bea1358fdb71848ecbb80f1c1c9baf157cbba76
Erfan <143827987+erfjab@users.noreply.github.com>
2025-01-10 03:15:08 +0330

    feat: add users active and admin actions (#133)
    * feat: add users active and admin actions
    
    * chore: change actions menu text

--- app/routers/actions/menu.py
+++ app/routers/actions/menu.py
@@ -21,14 +21,16 @@ async def data(callback: CallbackQuery, callback_data: PageCB):
         return await tracker.add(track)
 
     return await callback.message.edit_text(
-        text=MessageTexts.ITEMS_MENU,
+        text=MessageTexts.ITEMS,
         reply_markup=BotKeys.selector(
             data=[
                 ActionTypes.ADD_CONFIG,
                 ActionTypes.DELETE_CONFIG,
+                ActionTypes.ACTIVATED_USERS,
+                ActionTypes.DISABLED_USERS,
+                ActionTypes.DELETE_USERS,
                 ActionTypes.DELETE_EXPIRED_USERS,
                 ActionTypes.DELETE_LIMITED_USERS,
-                ActionTypes.DELETE_USERS,
             ],
             types=Pages.ACTIONS,
             action=Actions.INFO,
