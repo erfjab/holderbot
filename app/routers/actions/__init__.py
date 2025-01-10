9279de2f603054d7f9ec9da5432e4a302339fb9e
erfjab <erfjab@gmail.com>
2025-01-10 14:04:34 +0330

    feat: add admin users transfer

--- app/routers/actions/__init__.py
+++ app/routers/actions/__init__.py
@@ -1,9 +1,9 @@
 from aiogram import Router
 
 from . import menu
-from .items import configs, users
+from .items import configs, users, admin
 
-__all__ = ["setup_action_routers", "menu", "configs", "users"]
+__all__ = ["setup_action_routers", "menu", "configs", "users", "admin"]
 
 
 def setup_action_routers() -> Router:
@@ -12,5 +12,6 @@ def setup_action_routers() -> Router:
     router.include_router(menu.router)
     router.include_router(configs.router)
     router.include_router(users.router)
+    router.include_router(admin.router)
 
     return router
